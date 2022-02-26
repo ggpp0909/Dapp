3ea4c2c68167ea91d349851ecd7a12b68c0a8e7b
장영남 <ggpp0909@naver.com>
2022-02-26 20:16:37 +0900

    MyAnimalCard 컴포넌트 작성 2

--- inflearn_DappProject/frontend/src/components/MyAnimalCard.tsx
+++ inflearn_DappProject/frontend/src/components/MyAnimalCard.tsx
@@ -1,7 +1,7 @@
-import React, { FC } from "react";
-import { Box, Text } from "@chakra-ui/react"
+import React, { ChangeEvent, FC, useState } from "react";
+import { Box, Button, Input, InputGroup, InputRightAddon, Text } from "@chakra-ui/react"
 import AnimalCard from "./AnimalCard";
-import { web3 } from "../web3Config";
+import { saleAnimalTokenContract, web3 } from "../web3Config";
 
 export interface IMyAnimalCard {
   animalTokenId: string;
@@ -15,14 +15,49 @@ interface MyAnimalCardProps extends IMyAnimalCard {
 }
 
 const MyAnimalCard:FC<MyAnimalCardProps> = ({animalType, animalPrice, animalTokenId, account, saleStatus}) => {
+  const [sellPrice, setSellPrice] = useState<string>("");
+  const [myAnimalPrice, setMyAnimalPrice] = useState<string>(animalPrice);
+
+  const onChangeSellPrice = (e: ChangeEvent<HTMLInputElement>) => {
+    setSellPrice(e.target.value);
+  };
+
+  const onClickSell = async () => {
+    try {
+      if(!account || !saleStatus) return;
+      const response = await saleAnimalTokenContract.methods
+        .setForSaleAnimalToken(
+          animalTokenId,
+          web3.utils.toWei(sellPrice, "ether")
+        )
+        .send({ from: account });
+      
+      if (response.status) {
+        setMyAnimalPrice(web3.utils.toWei(sellPrice, "ether"));
+      }
+      console.log(response)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
   return (
     <Box textAlign="center" w={150}>
       <AnimalCard animalType={animalType}></AnimalCard>
       <Box mt={2}>
-        {animalPrice === "0"? (
-          <div>판매버튼</div>
+        {myAnimalPrice === "0"? (
+          <>
+            <InputGroup>
+              <Input type="number" value={sellPrice} onChange={onChangeSellPrice}></Input>
+              <InputRightAddon children="Ether"></InputRightAddon>
+            </InputGroup>
+            <Button size="sm" colorScheme="green" mt={2} onClick={onClickSell}>
+              Sell
+            </Button>
+          </>
         ) : (
-          <Text display="inline-block">{web3.utils.fromWei(animalPrice)} ether</Text>
+          <Text display="inline-block">{web3.utils.fromWei(myAnimalPrice)} Ether</Text>
         )}
       </Box>
     </Box>
